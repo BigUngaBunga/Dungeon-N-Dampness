c705b789599b30662b3f55e3ca37defea31ce3ac
BigUngaBunga <truls.rockstrom.ystad.nu@gmail.com>
2023-05-22 11:10:42 +0200

    Initial commit

---
+++ app/src/test/java/big/knaaledge/dungeon_n_dampness/ExampleUnitTest.kt
@@ -0,0 +1,17 @@
+package big.knaaledge.dungeon_n_dampness
+
+import org.junit.Test
+
+import org.junit.Assert.*
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+class ExampleUnitTest {
+    @Test
+    fun addition_isCorrect() {
+        assertEquals(4, 2 + 2)
+    }
+}

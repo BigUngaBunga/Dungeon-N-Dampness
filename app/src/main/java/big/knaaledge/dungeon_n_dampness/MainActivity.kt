d26d3dfce7ec93ba217b97e1c9f0f3d503ab3474
BigUngaBunga <truls.rockstrom.ystad.nu@gmail.com>
2023-05-24 16:24:05 +0200

    Remember messages & functional buttons
    Messages are now spearated. Buttons can be pressed and the console can be cleared. A queue works to feed new output to the console

--- app/src/main/java/big/knaaledge/dungeon_n_dampness/MainActivity.kt
+++ app/src/main/java/big/knaaledge/dungeon_n_dampness/MainActivity.kt
@@ -1,30 +1,29 @@
 package big.knaaledge.dungeon_n_dampness
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.*
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.MutableState
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import big.knaaledge.dungeon_n_dampness.components.GetActionButton
 import big.knaaledge.dungeon_n_dampness.components.SlowText
 import big.knaaledge.dungeon_n_dampness.data.ActionItem
 import big.knaaledge.dungeon_n_dampness.ui.theme.DungeonNDampnessTheme
-import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.*
 
+var output = mutableStateListOf<String>()
+var messageQueue = mutableStateListOf<String>()
+var readLines = mutableStateOf(0)
+var possibleActions = mutableStateListOf<ActionItem>()
 
-//var outputList by mutableStateOf<MutableList<String>>()
-
-lateinit var output: MutableList<String>
-lateinit var addedSymbols: MutableState<Int>
-lateinit var possibleActions: MutableList<ActionItem>
-
+var addedInitialText = mutableStateOf(false)
+var addedInitialActions = mutableStateOf(false)
 
 class MainActivity : ComponentActivity() {
 
@@ -32,8 +31,9 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
 
         setContent {
-            possibleActions = remember { mutableListOf() }
             AddInitialText()
+            AddInitialActions()
+
             DungeonNDampnessTheme {
                 Surface(
                     modifier = Modifier.fillMaxSize(),
@@ -47,93 +47,117 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+//region Console output
 @Composable
 fun AddInitialText(){
-    addedSymbols = remember { mutableStateOf(-1) }
-    output = remember { mutableListOf() }
-    output.add("Welcome to Dungeon & Dampness.")
-    output.add("You awaken in a damp cell. You are scheduled for execution. With a slight preference for living you decide to escape instead.")
-    output.add("You wave to the game")
-    output.add("The game waves back")
+    if(addedInitialText.value)
+        return
+    addedInitialText.value = true
+    messageQueue.add("Welcome to Dungeon & Dampness.")
+    messageQueue.add("You awaken in a damp cell. You are scheduled for execution. With a slight preference for living you decide to escape instead.")
+    messageQueue.add("You wave to the game")
+    messageQueue.add("The game waves back")
     for (i in 1..3){
-        output.add("Reeeee")
+        messageQueue.add("Reeeee")
     }
 }
 
 @Composable
 fun PrintOutput() {
+    if (output.size <= 0)
+        DequeueMessage()
     LazyColumn(
         Modifier
             .padding(16.dp)
             .padding(0.dp, 0.dp, 0.dp, 200.dp)){
-        val text = ListToString()
-        var readSymbols = text.length - addedSymbols.value
-        if (addedSymbols.value == -1)
-            readSymbols = 0
-        item { SlowText(text, readSymbols) }
+        items(output){message ->
+                SlowText(message = message, readSlowly = readLines.value <= output.lastIndexOf(message),
+                    onFinishedWriting = {DequeueMessage(); readLines.value++ })
+        }
     }
 }
 
-fun UpdateOutput(newLine : String){
-    addedSymbols.value = newLine.length
-    output.add(newLine)
+fun DequeueMessage(){
+    if (messageQueue.size <= 0)
+        return
+    var message = messageQueue.first()
+    messageQueue.removeFirst()
+    output.add("> $message")
+}
+
+fun EnqueueMessage(message: String){
+    messageQueue.add(message)
+    if (readLines.value >= output.size)
+        DequeueMessage()
+}
+
+fun ClearOutput(){
+    messageQueue.clear()
+    output.clear()
+    readLines.value = 0
+}
+
+fun <T>ClearOutput(vararg messages: T){
+    messageQueue.clear()
+    output.clear()
+    readLines.value = 0
+
+    CoroutineScope(Dispatchers.Main).launch {
+        delay(100)
+        for (t in messages)
+            EnqueueMessage(t.toString())
+
+    }
+}
+//endregion
+
+//region Action Buttons
+@Composable
+fun AddButtons(){
+    var actionsIncluded = 0
+    val numberOfRows = 3
+    val containerModifier = Modifier.fillMaxWidth()
+
+    Column(verticalArrangement = Arrangement.Bottom, modifier = containerModifier) {
+        for (i in 1..numberOfRows){
+            Row(horizontalArrangement = Arrangement.SpaceEvenly, modifier = containerModifier.padding(1.dp, 4.dp)) {
+                val buttonModifier = Modifier.weight(0.25f).height(64.dp).padding(2.dp)
+                AddActionButton(actionsIncluded++, buttonModifier)
+                AddActionButton(actionsIncluded++, buttonModifier)
+            }
+        }
+    }
 }
 
 @Composable
-fun AddButtons(){
-    val numberOfRows = 3
-    val modifier = Modifier.fillMaxWidth()
-
-
-    Column(verticalArrangement = Arrangement.Bottom, modifier = modifier) {
-//        for (i in 1..numberOfRows){
-//            Row(horizontalArrangement = Arrangement.SpaceEvenly, modifier = modifier.padding(1.dp, 4.dp)) {
-//                var buttonModifier = Modifier.weight(0.25f).height(64.dp).padding(2.dp)
-//                GetActionButton("", buttonModifier)
-//                GetActionButton("'Ello", buttonModifier)
-//            }
-//        }
-
-        Row(horizontalArrangement = Arrangement.SpaceEvenly, modifier = modifier.padding(1.dp, 4.dp)) {
-            val buttonModifier = Modifier
-                .weight(0.25f)
-                .height(64.dp)
-                .padding(2.dp)
-            GetActionButton(
-                ActionItem("Check inventory", action = { output.add("Inventoooory")}),
-                buttonModifier)
-            GetActionButton(
-                ActionItem("Scrounge around", action = {}),
-                buttonModifier)
-        }
-        Row(horizontalArrangement = Arrangement.SpaceEvenly, modifier = modifier.padding(1.dp, 4.dp)) {
-            val buttonModifier = Modifier
-                .weight(0.25f)
-                .height(64.dp)
-                .padding(2.dp)
-            GetActionButton(ActionItem("Watchu want??!?"), buttonModifier)
-            GetActionButton(modifier =  buttonModifier)
-        }
-        Row(horizontalArrangement = Arrangement.SpaceEvenly, modifier = modifier.padding(1.dp, 4.dp)) {
-            val buttonModifier = Modifier
-                .weight(0.25f)
-                .height(64.dp)
-                .padding(2.dp)
-            GetActionButton(modifier =  buttonModifier)
-            GetActionButton(modifier =  buttonModifier)
-        }
-    }
+fun AddActionButton(actionIdex: Int, modifier: Modifier){
+    if (actionIdex > possibleActions.size - 1)
+        GetActionButton(modifier = modifier)
+    else
+        GetActionButton(possibleActions[actionIdex], modifier)
 }
 
-fun ListToString(): String{
-    val stringBuilder = java.lang.StringBuilder()
-    for (message in output){
-        stringBuilder.append("> ")
-        stringBuilder.append(message)
-        stringBuilder.appendLine()
-        stringBuilder.appendLine()
-    }
+fun AddInitialActions(){
+    if(addedInitialActions.value)
+        return
+    addedInitialActions.value = true
+    var inventoryAction = ActionItem( message = "Check inventory",
+        description = "It's a wonder your pockets still hold up",
+        action = { EnqueueMessage("Inventoooory")})
+    inventoryAction.action = { EnqueueMessage(inventoryAction.description) }
 
-    return stringBuilder.toString()
+    possibleActions.add(inventoryAction)
+    possibleActions.add(
+        ActionItem(message = "Spin around",
+            description = "You spin right round till' you're dizzy",
+            action = {EnqueueMessage("You spin right round till' you're dizzy")}))
+    possibleActions.add(
+        ActionItem(message = "Go to sleep",
+            description = "The world fades to black",
+            action = { ClearOutput("The world fades to black",
+                "You wake up in the middle of nowhere")
+            }))
 }
+
+//endregion
 
--- app/src/main/java/big/knaaledge/dungeon_n_dampness/components/SlowText.kt
+++ app/src/main/java/big/knaaledge/dungeon_n_dampness/components/SlowText.kt
@@ -1,21 +1,28 @@
 package big.knaaledge.dungeon_n_dampness.components
 
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Text
 import androidx.compose.runtime.*
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import kotlinx.coroutines.coroutineScope
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 
 class SlowTextContainer(val message: String,
                         val output: MutableState<String>){
-    suspend fun UpdateTextSlowly(readSymbols: Int,
+    suspend fun UpdateTextSlowly(readSlowly: Boolean,
                                  delayTime : Long,
                                  newLineFactor: Long = 3,
                                  punctuationFactor : Long = 3){
-        var symbolsToSkip = readSymbols
+
+        if (!readSlowly){
+            output.value = message
+            return
+        }
+
         for (value in message){
             output.value = output.value.plus(value)
-            if (symbolsToSkip-- > 0){ continue }
             if(value == '>')
                 delay(delayTime * newLineFactor)
             else if (value == '.' || value == '?' || value == '!')
@@ -28,16 +35,16 @@ class SlowTextContainer(val message: String,
 
 
 @Composable
-fun SlowText(message : String, readSymbols : Int = 0, onFinishedWriting: () -> Unit = {}) {
+fun SlowText(message : String, readSlowly : Boolean = true, onFinishedWriting: () -> Unit = {}) {
     val output = remember{ mutableStateOf("")}
     var textContainer = remember { SlowTextContainer(message, output) }
 
-    Text(text = "${output.value}")//, lineHeight = 26.sp
+    Text(text = "${output.value}", modifier = Modifier.padding(0.dp, 2.dp))//, lineHeight = 26.sp
 
     LaunchedEffect(key1 = textContainer){
         coroutineScope{
             var writingJob = launch{
-                textContainer.UpdateTextSlowly(readSymbols = readSymbols,
+                textContainer.UpdateTextSlowly(readSlowly = readSlowly,
                                                 delayTime = 42, newLineFactor = 5)}
             writingJob.join()
             onFinishedWriting()
